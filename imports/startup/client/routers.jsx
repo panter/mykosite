acf4aa177a8ccd9ddad5c5deebc0665be80f00f6
Flavio Pellanda <fla@panter.ch>
2016-05-22 17:38:42 +0200

    Add quill class

--- imports/startup/client/routers.jsx
+++ imports/startup/client/routers.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {mount} from 'react-mounter';
 import AppContainer from '/imports/ui/layouts/AppContainer.jsx';
-import Content from '/imports/ui/components/content.jsx';
+import Quill from '/imports/ui/components/quill.jsx' 
 
 FlowRouter.route("/", {
   action () {
     mount(AppContainer, {
-      content: <Content name="Dominique" />
+      content: <Quill/>
     });
   }
 });
---
+++ imports/ui/components/quill.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactQuill from 'react-quill';
+
+export default Quill = ({name}) => (
+  <div>
+      <div>
+          <ReactQuill theme="snow" value='lksdjf'/>
+      </div>
+  </div>
+);
+
