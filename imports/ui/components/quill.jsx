a1a69188db275622a20a398f2cd0bb8285b433c3
Flavio Pellanda <fla@panter.ch>
2016-05-22 18:17:52 +0200

    Put quill on card

--- imports/ui/components/quill.jsx
+++ imports/ui/components/quill.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactQuill from 'react-quill';
-import {RaisedButton} from 'material-ui';
+import {RaisedButton, Card, CardText, CardActions} from 'material-ui';
 
 var onTextChange = function(value) {
   console.log(value);
 }
 
 export default Quill = ({name}) => (
-  <div>
-    <ReactQuill theme="snow" value='Welcome to text to internet' onChange={onTextChange}/>
-    <div className="bottom">
+  <Card className="quill">
+    <CardText>
+      <ReactQuill theme="snow" value='Welcome to text to internet' onChange={onTextChange}/>
+    </CardText>
+    <CardActions className="bottom">
       <RaisedButton label="Speichern" primary={true} />
       <RaisedButton label="Abbrechen" secondary={true} />
-    </div>
-  </div>
+    </CardActions>
+  </Card>
 )
