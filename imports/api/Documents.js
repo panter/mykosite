be4f52526a1790cc487a22f2444bde8d65c54249
Dominique Schuwey <domi@domidesign.ch>
2016-05-22 16:49:19 +0200

    wip

---
+++ imports/api/Documents.js
@@ -0,0 +1,29 @@
+import { Mongo } from 'meteor/mongo';
+
+export const Documents = new Mongo.Collection('documents');
+Documents.attachSchema({
+  createdAt: {
+    type: Date,
+    index: 1,
+    autoform: {
+      type: "datetime"
+    },
+    autoValue() {
+      if (this.isInsert) {
+        return new Date;
+      } else if (this.isUpsert) {
+        return {$setOnInsert: new Date};
+      } else {
+        this.unset();  // Prevent user from supplying their own value
+      }
+    }
+  },
+  name: {
+    type: String,
+    defaultValue: 'document-name'
+  },
+  text: {
+    type: String,
+    defaultValue: 'Here goes your text'
+  }
+});
--- server/main.js
+++ server/main.js
@@ -1,1 +1,2 @@
 import '../imports/startup/server/index.jsx'
+import '../imports/api/tasks.js';
