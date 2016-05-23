cb194d2efde471eb20231a7a47175d6fde055d6f
Flavio Pellanda <fla@panter.ch>
2016-05-24 00:07:06 +0200

    Add share buttons with icons

--- imports/ui/components/links.jsx
+++ imports/ui/components/links.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import {Paper} from 'material-ui';
+import { FacebookCount } from "react-social";
+// see https://github.com/olahol/react-social
+import { FacebookButton, TwitterButton, GooglePlusButton, PinterestButton, LinkedInButton, RedditButton, VKontakteButton, EmailButton, XingButton, TumblrButton} from "react-social"
+// see http://markhuge.github.io/svg-social-icons/
+import icons from 'svg-social-icons' 
 
 const link = function (document, editable) {
   var path = '?' + document.name
@@ -21,6 +26,36 @@ const Links = ({document}) => {
       &nbsp; &nbsp; &nbsp;
       <span>
         Edit link: &nbsp; {link(document, true)}
+      </span>
+      <br/>
+      <span className="share-buttons">
+          <FacebookButton>
+              <div dangerouslySetInnerHTML={{__html: icons('facebook')}}/>
+          </FacebookButton>
+          <TwitterButton>
+              <div dangerouslySetInnerHTML={{__html: icons('twitter')}}/>
+          </TwitterButton>
+          <GooglePlusButton>
+              <div dangerouslySetInnerHTML={{__html: icons('google')}}/>
+          </GooglePlusButton>
+          <PinterestButton>
+              <div dangerouslySetInnerHTML={{__html: icons('pinterest')}}/>
+          </PinterestButton>
+          <LinkedInButton>
+              <div dangerouslySetInnerHTML={{__html: icons('linkedin')}}/>
+          </LinkedInButton>
+          {/**<RedditButton>
+              <div dangerouslySetInnerHTML={{__html: icons('reddit')}}/>
+          </RedditButton> **/}
+          <EmailButton>
+              <div dangerouslySetInnerHTML={{__html: icons('email')}}/>
+          </EmailButton>
+          {/**<<XingButton>
+              <div dangerouslySetInnerHTML={{__html: icons('xing')}}/>
+          </XingButton> **/}
+          <TumblrButton>
+              <div dangerouslySetInnerHTML={{__html: icons('tumblr')}}/>
+          </TumblrButton>
       </span>
   </Paper>)
 }
