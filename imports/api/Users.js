e50e1a1091ad857458efdecb95feca76ed362367
Flavio Pellanda <fla@panter.ch>
2016-05-24 18:13:04 +0200

    Fix editor

--- imports/api/Users.js
+++ imports/api/Users.js
@@ -5,15 +5,17 @@ if (Meteor.isClient) {
   Meteor.subscribe('userData');
 }
 
-Meteor.publish('userData', function() {
-  if(!this.userId) return null;
-  return Meteor.users.find(this.userId, {fields: {
-    tokens: 1,
-  }});
-});
+if (Meteor.isServer) {
+  Meteor.publish('userData', function() {
+    if(!this.userId) return null;
+    return Meteor.users.find(this.userId, {fields: {
+      tokens: 1,
+    }});
+  });
 
-Meteor.methods({
-  'user.addToken'(token) {
-     Meteor.users.update({ _id: this.userId }, { $addToSet: { tokens: token }});
-   },
-})
+  Meteor.methods({
+    'user.addToken'(token) {
+      Meteor.users.update({ _id: this.userId }, { $addToSet: { tokens: token }});
+    },
+  })
+}
