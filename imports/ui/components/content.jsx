19b587cefce50175debeba5c804edcb4f6c4a9c6
Flavio Pellanda <fla@panter.ch>
2016-05-26 12:00:45 +0200

    Remove obsolete file

--- imports/ui/components/content.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default Content = ({name}) => (
-<div>
-  <div className="section" id="index-banner">
-    <h3 className="header center">Put your text into the internet:</h3>
-  </div>
-</div>
-);
