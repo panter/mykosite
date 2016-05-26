8d3db6d9951cd707f951ec9c2ed9397d6a839044
Philip Graf <phg@panter.ch>
2016-05-26 13:57:17 +0300

    Fix page title

--- imports/ui/containers/page.js
+++ imports/ui/containers/page.js
@@ -3,9 +3,10 @@ import Page from '/imports/ui/components/page.jsx'
 import { Documents } from '/imports/api/Documents.js'
 
 function composer (props, onData) {
-  const handle = Subs.subscribe('documentsByName', Session.get('documentName'));
+  const name = Session.get('documentName');
+  const handle = Subs.subscribe('documentsByName', name);
   if (handle.ready()) {
-    const document = Documents.findOne({ name: Session.get('documentName') });
+    const document = Documents.findOne({ name: name });
     onData(null, {document, name});
   }
 }
